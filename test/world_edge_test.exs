89cc909b648fde5f28ff131085910914082e761e
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-06-05 13:18:11 +0200

    Add World edge test

--- test/world_edge_test.exs
+++ test/world_edge_test.exs
@@ -1,15 +1,43 @@
 defmodule WorldEdgeTest do
   use ExUnit.Case, async: true
 
-  describe "1x1 world wraps immediately" do
+  ### 2x2 Board coordinate map
+  # 0,1 1,1
+  # 0,0 1,0
+  ###
+
+  @tag :skip
+  test "1x1 world wraps immediately" do
     # GIVEN Rover is placed into a 1x1 world
     # WHEN Rover moves forward
     # THEN Rover does not change position
   end
 
-  describe "2x2 world wraps around the edges" do
+  @tag :skip
+  test "2x2 world wraps from top to bottom" do
     # GIVEN Rover is placed into a 2x2 world at position 1,1 facing north
     # WHEN Rover moves forward
     # THEN Rover wraps to the bottom of the board (1,0)
+  end
+
+  @tag :skip
+  test "2x2 world wraps from right to left" do
+    # GIVEN Rover is placed into a 2x2 world at position 1,1 facing east
+    # WHEN Rover moves forward
+    # THEN Rover wraps to the left of the board (0,1)
+  end
+
+  @tag :skip
+  test "2x2 world wraps from bottom to top" do
+    # GIVEN Rover is placed into a 2x2 world at position 0,0 facing south
+    # WHEN Rover moves forward
+    # THEN Rover wraps to the left of the board (0,1)
+  end
+
+  @tag :skip
+  test "2x2 world wraps from left to right" do
+    # GIVEN Rover is placed into a 2x2 world at position 0,0 facing west
+    # WHEN Rover moves forward
+    # THEN Rover wraps to the right of the board (1,0)
   end
 end
